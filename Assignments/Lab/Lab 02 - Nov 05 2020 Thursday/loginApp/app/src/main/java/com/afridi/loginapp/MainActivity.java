2b2eb4077fa12903e8f76ff1f26f6d252969c9b6
afridi171 <afridi.hasan@northsouth.edu>
2020-11-15 08:10:11 +0600

    Lab2: LoginApp: added image view and added toast message

--- Assignments/Lab/Lab 02 - Nov 05 2020 Thursday/loginApp/app/src/main/java/com/afridi/loginapp/MainActivity.java
+++ Assignments/Lab/Lab 02 - Nov 05 2020 Thursday/loginApp/app/src/main/java/com/afridi/loginapp/MainActivity.java
@@ -2,19 +2,30 @@ package com.afridi.loginapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.widget.Button;
 import android.os.Bundle;
-import android.view.View;
+import android.widget.Toast;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-    EditText in1 = (EditText)findViewById(R.id.editTextTextPersonName);
-    EditText in2 = (EditText)findViewById(R.id.editTextTextPassword);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-    public void login(View view){
 
+        EditText inUsername = findViewById(R.id.etUsername);
+        EditText inPassword = findViewById(R.id.etPassword);
+        Button inLogin = findViewById(R.id.btnLogin);
+
+        inLogin.setOnClickListener(v -> {
+            String username = inUsername.getText().toString();
+            String password =   inPassword.getText().toString();
+
+            if(username.equals("user") && password.equals("1234")) {
+                Toast.makeText(MainActivity.this, "Successful", Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(MainActivity.this, "Failed", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
