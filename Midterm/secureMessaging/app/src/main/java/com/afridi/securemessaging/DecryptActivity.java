9c0e36bac1f7cf3a576371ed896af7f7ce9c8bdc
afridi171 <afridi.hasan@northsouth.edu>
2020-12-28 23:27:27 +0600

    Decryption activity completed

--- Midterm/secureMessaging/app/src/main/java/com/afridi/securemessaging/DecryptActivity.java
+++ Midterm/secureMessaging/app/src/main/java/com/afridi/securemessaging/DecryptActivity.java
@@ -2,13 +2,50 @@ package com.afridi.securemessaging;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
 
 public class DecryptActivity extends AppCompatActivity {
-
+    Crypto crypto;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_decrypt2);
+
+        crypto = new Crypto();
+        EditText EncryptedSms = (EditText)findViewById(R.id.etEncryptedSMS);
+        Button btnDecryption = (Button)findViewById(R.id.decryptButton);
+        Button btnEncryption = (Button)findViewById(R.id.encryptButton);
+        TextView sms = (TextView)findViewById(R.id.tvDecryptedSMS);
+
+
+        btnDecryption.setOnClickListener(v -> {
+            String cypheredSms = EncryptedSms.getText().toString();
+
+            if (cypheredSms.isEmpty()){
+                btnDecryption.setError("");
+                return;
+            }
+            
+            String[] str = cypheredSms.split(" ",2);
+            String pKey =str[0];
+            String eSms = str[1];
+
+            try {
+                String actualSms = crypto.decrypt(eSms, Integer.parseInt(pKey));
+                sms.setText(actualSms );
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+        btnEncryption.setOnClickListener(v -> {
+            Intent intent = new Intent(DecryptActivity.this, MainActivity.class);
+            startActivity(intent);
+        });
     }
 }
