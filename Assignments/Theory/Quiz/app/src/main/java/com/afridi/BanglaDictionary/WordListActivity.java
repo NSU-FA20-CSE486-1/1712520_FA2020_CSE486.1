c71a9d802769a2032bc0260c657ac83dc8df5dba
afridi171 <afridi.hasan@northsouth.edu>
2021-01-10 21:54:53 +0600

    #3 Activity layout completed

---
+++ Assignments/Theory/Quiz/app/src/main/java/com/afridi/BanglaDictionary/WordListActivity.java
@@ -0,0 +1,14 @@
+package com.afridi.BanglaDictionary;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class WordListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_word_list);
+    }
+}
