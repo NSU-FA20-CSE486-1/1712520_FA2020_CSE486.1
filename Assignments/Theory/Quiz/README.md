92c089cf0c094b0bcc48f40b12f8e79c8deaf1ca
afridi171 <afridi.hasan@northsouth.edu>
2021-01-17 00:21:49 +0600

    #3 dictionary updated

---
+++ Assignments/Theory/Quiz/README.md
@@ -0,0 +1,22 @@
+Quiz 2: Bangla Dictionary
+
+> Add Word Activity
+
+<img src="1.jpg" width="220" height="400"/>
+
+>Empty Field Showing Error
+
+<img src="2.jpg" width="200" height="400"/>
+
+>New Word Added With Meaning
+
+<img src="3.jpg" width="200" height="400"/>
+
+>Word List
+
+<img src="4.jpg" width="200" height="400"/>
+
+
+>Word Meaning in Custom Dialog Box
+
+<img src="5.jpg" width="200" height="400"/>
--- Assignments/Theory/Quiz/app/src/main/java/com/afridi/BanglaDictionary/MainActivity.java
+++ Assignments/Theory/Quiz/app/src/main/java/com/afridi/BanglaDictionary/MainActivity.java
@@ -1,28 +1,14 @@
 package com.afridi.BanglaDictionary;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.ContextCompat;
 
-import android.graphics.PorterDuff;
-import android.graphics.PorterDuffColorFilter;
-import android.graphics.drawable.Drawable;
-import android.graphics.drawable.GradientDrawable;
 import android.os.Bundle;
-import android.widget.TextView;
-
-import static java.security.AccessController.getContext;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView mBanglaDictionary;
-    Drawable mDrawable;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        mBanglaDictionary = (TextView)findViewById(R.id.tvBanglaDictionary);
-
-
     }
 }
